eb2bf7045327e5f14014c18fb66ab90ee14f7a5e
jaehong21 <dev@jaehong21.com>
2025-02-27 14:33:50 +0900

    feat: add Kitty terminal configuration for macOS

--- home/mac/default.nix
+++ home/mac/default.nix
@@ -6,10 +6,10 @@
     ./direnv.nix
     ./git.nix
     ./zsh.nix
+    ./kitty.nix
     ./starship.nix
     ./neovim.nix
     ./lang.nix
     ./kubernetes.nix
   ];
 }
-
---
+++ home/mac/kitty.nix
@@ -0,0 +1,70 @@
+{ config, pkgs, ... }:
+
+{
+  programs.kitty = {
+    enable = true;
+    shellIntegration = {
+      enableBashIntegration = true;
+      enableZshIntegration = true;
+    };
+    font = {
+      name = "Hack Nerd Font";
+      size = 15.5;
+    };
+    # from https://github.com/kovidgoyal/kitty-themes/tree/master/themes
+    themeFile = "kanagawa";
+    keybindings = {
+      # Window Management
+      "cmd+d" = "launch --location=vsplit --cwd=current";
+      "cmd+j" = "launch --location=hsplit --cwd=current";
+
+      # Window Navigation
+      "cmd+]" = "next_window";
+      "cmd+[" = "previous_window";
+
+      # Window Resizing
+      "opt+j" = "resize_window shorter 2";
+      "opt+k" = "resize_window taller 2";
+      "opt+h" = "resize_window wider 2";
+      "opt+l" = "resize_window narrower 2";
+
+      # Window Actions
+      "cmd+w" = "close_window";
+      "cmd+o" = "open_url_with_hints";
+      "cmd+f" = "toggle_fullscreen";
+      "cmd+shift+r" = "load_config_file";
+    };
+    settings = {
+      # Window Layout
+      enabled_layouts = "splits";
+      initial_window_width = 1200;
+      initial_window_height = 800;
+      window_padding_width = 4;
+
+      # Tab Bar
+      tab_bar_edge = "top";
+      tab_bar_style = "powerline";
+      tab_powerline_style = "slanted";
+      tab_bar_min_tabs = 1;
+      tab_title_template = "{index}.{tab.active_wd.replace('${config.home.homeDirectory}', '~').split('/')[-1]}";
+
+      # Font Configuration
+      bold_font = "auto";
+      italic_font = "auto";
+      bold_italic_font = "auto";
+      modify_font = "cell_height +1px";
+
+      # OS Integration
+      confirm_os_window_close = 0;
+      macos_titlebar_color = "background";
+      clipboard_max_size = 0;
+
+      # Background
+      background_image_layout = "cscaled";
+
+      # Text Cursor Customization
+      cursor_trail = 2;
+      cursor_trail_decay = "0.1 0.2";
+    };
+  };
+}
