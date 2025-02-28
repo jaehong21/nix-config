033c8a7f8ee5f54914aca2bf8a3285585fc1e0cc
jaehong21 <dev@jaehong21.com>
2025-02-28 19:45:15 +0900

    config: set terraform to 1.7.4

--- home/mac/lang.nix
+++ home/mac/lang.nix
@@ -1,18 +1,19 @@
 { config, pkgs, ... }:
 
 let
-  terraform174 =  import (pkgs.fetchFromGitHub {
-    owner = "NixOS";
-    repo = "nixpkgs";
-    rev = "a343533bccc62400e8a9560423486a3b6c11a23b";
-    hash = "sha256-TofHtnlrOBCxtSZ9nnlsTybDnQXUmQrlIleXF1RQAwQ=";
-  }) {
-    config.allowUnfree = true;
-    inherit (pkgs) system;
-  };
+  terraform = import
+    (pkgs.fetchFromGitHub {
+      owner = "NixOS";
+      repo = "nixpkgs";
+      rev = "a343533bccc62400e8a9560423486a3b6c11a23b";
+      hash = "sha256-TofHtnlrOBCxtSZ9nnlsTybDnQXUmQrlIleXF1RQAwQ=";
+    })
+    {
+      config.allowUnfree = true;
+      inherit (pkgs) system;
+    };
 in
 {
-  # home.packages = with pkgsWithOverlay; [
   home.packages = with pkgs; [
     bun
     nodejs_22
