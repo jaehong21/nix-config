c4762d3c1afc5a2f0eb2177bc51141bd00b54132
jaehong21 <dev@jaehong21.com>
2025-02-28 19:45:28 +0900

    docs: format comments

--- home/mac/direnv.nix
+++ home/mac/direnv.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ... }:
 
 {
-# https://nix-community.github.io/home-manager/options.xhtml#opt-programs.direnv.enable
+  # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.direnv.enable
   programs.direnv = {
     enable = true;
     nix-direnv.enable = true;
