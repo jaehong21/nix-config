c0a92bf8f699916b7fe657ead8610484d660444a
jaehong21 <dev@jaehong21.com>
2025-03-01 15:25:18 +0900

    feat: add NixOS configuration for berry2 host

--- flake.nix
+++ flake.nix
@@ -19,45 +19,60 @@
     sops-nix.url = "github:Mic92/sops-nix";
   };
 
-  outputs = { self, nixpkgs, darwin, home-manager, ... }@inputs:
-    {
-      homeConfigurations = {
-        # Work laptop
-        # <username>
-        "jetty" = home-manager.lib.homeManagerConfiguration {
-          pkgs = import nixpkgs {
-            system = "aarch64-darwin";
-            config.allowUnfree = true;
-          };
-          extraSpecialArgs = { inherit inputs self; }; # pass inputs to home.nix
-          modules = [
-            ./hosts/jetty/home.nix
-            {
-              home.username = "jetty";
-              home.homeDirectory = "/Users/jetty";
-            }
-          ];
-
-        };
-      };
-
-      # Build darwin flake using:
-      # $ darwin-rebuild build --flake .#jetty
-      darwinConfigurations = {
-        # <hostname>
-        jetty = darwin.lib.darwinSystem {
+  outputs = { self, nixpkgs, darwin, home-manager, ... }@inputs: {
+    homeConfigurations = {
+      # Work laptop
+      # <username>
+      "jetty" = home-manager.lib.homeManagerConfiguration {
+        pkgs = import nixpkgs {
           system = "aarch64-darwin";
-          modules = [
-            ./darwin
-            {
-              nixpkgs.hostPlatform = "aarch64-darwin";
-              users.users.jetty = {
-                name = "jetty";
-                home = "/Users/jetty";
-              };
-            }
-          ];
+          config.allowUnfree = true;
         };
+        extraSpecialArgs = { inherit inputs self; }; # pass inputs to home.nix
+        modules = [
+          ./hosts/jetty/home.nix
+          {
+            home.username = "jetty";
+            home.homeDirectory = "/Users/jetty";
+          }
+        ];
       };
     };
+
+    # Build nixos flake using:
+    # $ sudo nixos-rebuild build --flake .#berry2
+    nixosConfigurations = {
+      berry2 = nixpkgs.lib.nixosSystem {
+        system = "aarch64-linux";
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
+        specialArgs = { inherit inputs system; };
+        modules = [
+          # temp: NixOS configuration.nix config path
+          /etc/nixos/configuration.nix
+        ];
+      };
+    };
+
+    # Build darwin flake using:
+    # $ darwin-rebuild build --flake .#jetty
+    darwinConfigurations = {
+      # <hostname>
+      jetty = darwin.lib.darwinSystem {
+        system = "aarch64-darwin";
+        modules = [
+          ./darwin
+          {
+            nixpkgs.hostPlatform = "aarch64-darwin";
+            users.users.jetty = {
+              name = "jetty";
+              home = "/Users/jetty";
+            };
+          }
+        ];
+      };
+    };
+  };
 }
