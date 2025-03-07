889da8bf9f7b751fce66ed489a4c19d457336228
jaehong21 <dev@jaehong21.com>
2025-03-08 00:10:22 +0900

    feat: add oracle4 NixOS host configuration
    - Add NixOS configuration for oracle4 Oracle Cloud ARM instance
    - Configure k3s overlay with specific Nixpkgs revision
    - Update flake.nix to include oracle4 NixOS configuration
    - Set up system configuration for aarch64-linux architecture

--- hosts/oracle4/configuration.nix
+++ hosts/oracle4/configuration.nix
@@ -12,41 +12,35 @@ let
       { inherit (pkgs) system; }).k3s;
   };
 in
-{ nixpkgs.overlays = [
+{
+  nixpkgs.overlays = [
     k3sOverlay
   ];
 
   imports =
     [
-      # Include the results of the hardware scan.
       ./hardware-configuration.nix
+      inputs.disko.nixosModules.disko
+      ./disk-config.nix
       inputs.sops-nix.nixosModules.sops
     ];
 
-  # Use the systemd-boot EFI boot loader.
-  boot.loader.systemd-boot.enable = true;
+  boot = {
+    loader = {
+      systemd-boot.enable = true;
+      efi = {
+        canTouchEfiVariables = true;
+        efiSysMountPoint = "/boot";
+      };
+    };
+    initrd.systemd.enable = true;
+  };
 
-  # for Rpi 5
-  boot.loader.efi.canTouchEfiVariables = false; # default: true
-  boot.kernelPackages = pkgs.linuxPackages_rpi4;
-  # https://github.com/k3s-io/k3s/issues/2067#issuecomment-801710748
-  # https://github.com/NixOS/nixpkgs/blob/master/pkgs/applications/networking/cluster/k3s/docs/CLUSTER_UPKEEP.md#raspberry-pi-not-working
-  # for k3s service in Raspberry Pi
-  boot.kernelParams = [
-    "cgroup_enable=cpuset"
-    "cgroup_memory=1"
-    "cgroup_enable=memory"
-  ];
+  systemd.targets.multi-user.enable = true;
 
-  # disable swap
-  swapDevices = lib.mkForce [ ];
+  networking.hostName = "oracle4";
+  networking.networkmanager.enable = true;
 
-  networking.hostName = "berry2"; # Define your hostname.
-  # Pick only one of the below networking options.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-  networking.networkmanager.enable = true; # Easiest to use and most distros use this by default.
-
-  # Set your time zone.
   time.timeZone = "Asia/Seoul";
   i18n.defaultLocale = "en_US.UTF-8";
 
@@ -57,6 +51,7 @@ in
     defaultSopsFile = "${self.outPath}/secrets/encrypted.yaml";
     defaultSopsFormat = "yaml";
     # should have no passphrase
+    # NOTE: add `key.txt` manually
     age.keyFile = "/var/lib/sops-nix/key.txt";
 
     secrets = {
@@ -64,30 +59,27 @@ in
     };
   };
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.mutableUsers = false; # disable change password with `passwd`.
-  users.users.root.hashedPassword = "!";
-  users.users.jaehong21 = {
-    isNormalUser = true;
-    # initialPassword = "xxx";
-    hashedPassword = "!";
-    openssh.authorizedKeys.keys = [ (builtins.readFile ./id_rsa.pub) ];
-    extraGroups = [
-      "wheel"
-      "networkmanager"
-      "docker"
-    ];
+  users = {
+    mutableUsers = false;
+    users.jaehong21 = {
+      isNormalUser = true;
+      openssh.authorizedKeys.keys = [ (builtins.readFile ./id_rsa.pub) ];
+      extraGroups = [
+        "wheel"
+        "networkmanager"
+        "docker"
+      ];
 
-    packages = with pkgs; [
-      fastfetch
-      gh
-      gnupg
-      htop
-      nh
-      python310
-      tailscale # CLI
-      tree
-    ];
+      packages = with pkgs; [
+        fastfetch
+        gh
+        gnupg
+        nh
+        python310
+        tailscale # CLI
+        tree
+      ];
+    };
   };
 
   security.sudo.wheelNeedsPassword = false;
@@ -95,17 +87,15 @@ in
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
   environment.systemPackages = with pkgs; [
     curl
     dnsutils
     git
-    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    htop
+    vim
+    vnstat
     wget
   ];
-
-  # List services that you want to enable:
 
   # Google's NTP service instead systemd-timesyncd
   services.ntp = {
@@ -117,12 +107,9 @@ in
   # Enable the OpenSSH daemon.
   services.openssh = {
     enable = true;
-    ports = [ 22 ];
     settings = {
       PermitRootLogin = "no";
-      # disable for public key authentication
       PasswordAuthentication = false;
-      KbdInteractiveAuthentication = false;
     };
   };
 
@@ -140,27 +127,23 @@ in
   };
 
   # k3s agent
-  services.k3s = {
-    enable = true;
-    role = "agent";
-    tokenFile = "${config.sops.secrets."k3s/token".path}";
-    serverAddr = "https://kube.jaehong21.com:6443";
-    extraFlags = [
-      "--flannel-iface tailscale0"
-    ];
-  };
+  # services.k3s = {
+  #   enable = true;
+  #   role = "agent";
+  #   tokenFile = "${config.sops.secrets."k3s/token".path}";
+  #   serverAddr = "https://kube.jaehong21.com:6443";
+  #   extraFlags = [
+  #     "--flannel-iface tailscale0"
+  #   ];
+  # };
 
   # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  networking.firewall.enable = false;
+  networking.firewall.allowedTCPPorts = [ 22 80 443 6443 10250 ];
+  networking.firewall.allowedUDPPorts = [ 8472 ];
+  # networking.firewall.enable = false;
 
-
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
+  # Disable documentation for minimal install.
+  documentation.enable = false;
 
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
