2a4843e763cf9fd79c3fb64c66c3d30f00850205
jaehong21 <dev@jaehong21.com>
2025-03-12 23:53:14 +0900

    feat: add nfs-utils

--- hosts/berry1/configuration.nix
+++ hosts/berry1/configuration.nix
@@ -104,6 +104,7 @@ in
     curl
     dnsutils
     git
+    nfs-utils
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
   ];
--- hosts/berry3/configuration.nix
+++ hosts/berry3/configuration.nix
@@ -102,6 +102,7 @@ in
     curl
     dnsutils
     git
+    nfs-utils
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
   ];
