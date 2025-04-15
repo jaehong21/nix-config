ca17f2b494f7346b444937f0f37e5c838d8e9185
jaehong21 <dev@jaehong21.com>
2025-04-15 11:13:52 +0900

    feat: disable NetworkManager-wait-online

--- hosts/berry3/configuration.nix
+++ hosts/berry3/configuration.nix
@@ -46,6 +46,7 @@ in
   # Pick only one of the below networking options.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   networking.networkmanager.enable = true; # Easiest to use and most distros use this by default.
+  systemd.services.NetworkManager-wait-online.enable = false;
 
   # Set your time zone.
   time.timeZone = "Asia/Seoul";
