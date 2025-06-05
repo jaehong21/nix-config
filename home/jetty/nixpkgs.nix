2cdfe707d2293e9bf379290c5a066b87eaab7fce
jaehong21 <dev@jaehong21.com>
2025-06-05 11:03:23 +0900

    feat: install claude-code

--- home/jetty/nixpkgs.nix
+++ home/jetty/nixpkgs.nix
@@ -1,6 +1,25 @@
 { config, pkgs, ... }:
 
+let
+  claudeCodeOverlay = final: prev: {
+    claude_code_1_0_3 = (import
+      (pkgs.fetchFromGitHub {
+        owner = "NixOS";
+        repo = "nixpkgs";
+        rev = "bdc995d3e97cec29eacc8fbe87e66edfea26b861";
+        hash = "sha256-fTll03tzUcgBrrMvD6O06TittBG2Ae6m3iW7aunxwPY=";
+      })
+      {
+        config.allowUnfree = true;
+        inherit (pkgs) system;
+      }).claude-code;
+  };
+in
 {
+  nixpkgs.overlays = [
+    claudeCodeOverlay
+  ];
+
   # https://search.nixos.org/packages
   home.packages = with pkgs; [
     age
@@ -10,13 +29,16 @@
     argocd
     autojump
     awscli2
+    claude_code_1_0_3
     curl
     doggo
     fd
+    fx
     fzf
     gh
     gnupg
     go-migrate
+    go-swag
     golangci-lint
     gradle
     grpcurl
@@ -37,8 +59,8 @@
     sqlboiler
     step-ca
     stern
-    go-swag
     tree
+    tshark
     unixtools.watch
     uv
     wget
