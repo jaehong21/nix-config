be550721832b7c31da3fc86349aba3560e1e90ea
jaehong21 <dev@jaehong21.com>
2025-07-28 09:49:50 +0900

    feat: add cloudflare trax key as secret

--- home/jetty/env.nix
+++ home/jetty/env.nix
@@ -17,9 +17,10 @@
     TF_VAR_aws_trax_secret_key = "$(cat ${config.sops.secrets."aws/trax/secret_key".path})";
     TF_VAR_aws_nari_access_key = "$(cat ${config.sops.secrets."aws/nari/access_key".path})";
     TF_VAR_aws_nari_secret_key = "$(cat ${config.sops.secrets."aws/nari/secret_key".path})";
-    TF_VAR_cloudflare_api_key = "$(cat ${config.sops.secrets."cloudflare/api_key".path})";
-    TF_VAR_r2_access_key = "$(cat ${config.sops.secrets."cloudflare/r2/access_key".path})";
-    TF_VAR_r2_secret_key = "$(cat ${config.sops.secrets."cloudflare/r2/secret_key".path})";
+    TF_VAR_cloudflare_jaehong21_api_key = "$(cat ${config.sops.secrets."cloudflare/jaehong21/api_key".path})";
+    TF_VAR_cloudflare_jaehong21_r2_access_key = "$(cat ${config.sops.secrets."cloudflare/jaehong21/r2/access_key".path})";
+    TF_VAR_cloudflare_jaehong21_r2_secret_key = "$(cat ${config.sops.secrets."cloudflare/jaehong21/r2/secret_key".path})";
+    TF_VAR_cloudflare_trax_api_key = "$(cat ${config.sops.secrets."cloudflare/trax/api_key".path})";
     TF_VAR_oci_jaehong21_user_ocid = "$(cat ${config.sops.secrets."oci/jaehong21/user_ocid".path})";
     TF_VAR_oci_jaehong21_fingerprint = "$(cat ${config.sops.secrets."oci/jaehong21/fingerprint".path})";
     TF_VAR_oci_jaehong21_private_key_path = "${config.sops.secrets."oci/jaehong21/private_key".path}";
@@ -43,9 +44,10 @@
     "aws/nari/access_key" = { };
     "aws/nari/secret_key" = { };
     "channelio/linear/api_key" = { };
-    "cloudflare/api_key" = { };
-    "cloudflare/r2/access_key" = { };
-    "cloudflare/r2/secret_key" = { };
+    "cloudflare/jaehong21/api_key" = { };
+    "cloudflare/jaehong21/r2/access_key" = { };
+    "cloudflare/jaehong21/r2/secret_key" = { };
+    "cloudflare/trax/api_key" = { };
     "oci/jaehong21/user_ocid" = { };
     "oci/jaehong21/fingerprint" = { };
     "oci/jaehong21/private_key" = { };
