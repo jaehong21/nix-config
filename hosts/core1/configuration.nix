ae2ba6a062dbc93f35c343b2ceb825bfe06b96d0
jaehong21 <dev@jaehong21.com>
2026-04-19 13:37:22 +0900

    feat: update pocket-id to version v2.5.0

--- hosts/core1/configuration.nix
+++ hosts/core1/configuration.nix
@@ -239,7 +239,7 @@ in
         ];
       };
       pocket-id = {
-        image = "ghcr.io/pocket-id/pocket-id:v2.2.0";
+        image = "ghcr.io/pocket-id/pocket-id:v2.5.0";
         ports = [ "1411:1411" ];
         environment = {
           APP_URL = "https://id.jaehong21.com";
