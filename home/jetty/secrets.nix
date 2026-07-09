7a0686ae31a233d8a17026d1aade8101d8d99d48
jaehong21 <dev@jaehong21.com>
2026-07-09 21:49:54 +0900

    revert: add MINIMAX_API_KEY as secret

--- home/jetty/secrets.nix
+++ home/jetty/secrets.nix
@@ -5,7 +5,6 @@
     "github/token" = { };
 
     "openrouter/api_key" = { };
-    "minimax/api_key" = { };
     "groq/api_key" = { };
     "prism/codex/api_key" = { };
 
@@ -17,7 +16,6 @@
     GITHUB_TOKEN = "$(cat ${config.sops.secrets."github/token".path})";
     GITHUB_PACKAGES_INSTALL_KEY = "$(cat ${config.sops.secrets."github/token".path})";
     OPENROUTER_API_KEY = "$(cat ${config.sops.secrets."openrouter/api_key".path})";
-    MINIMAX_API_KEY = "$(cat ${config.sops.secrets."minimax/api_key".path})";
     GROQ_API_KEY = "$(cat ${config.sops.secrets."groq/api_key".path})";
     PRISM_CODEX_API_KEY = "$(cat ${config.sops.secrets."prism/codex/api_key".path})";
     LINEAR_API_KEY = "$(cat ${config.sops.secrets."linear/api_key".path})";
